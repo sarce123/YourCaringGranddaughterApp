d30b435e90f084d5be1b48c64695cb0cb2cba94c
Skylar A <skylar.arce@yahoo.com>
2021-07-22 12:13:50 -0700

    completed the third view controller (staying hip and fit)

--- YourCaringGranddaughterApp/ThirdViewController.swift
+++ YourCaringGranddaughterApp/ThirdViewController.swift
@@ -15,7 +15,29 @@ class ThirdViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func popsugarRecipes(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.popsugar.com/latest/Healthy-Recipes")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func exercisePlanButton(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.healthline.com/health/everyday-fitness/senior-workouts")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    
+    @IBAction func chairYogaVideo(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.youtube.com/watch?v=3ZvmKOPoFVo")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func pilatesVideo(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.youtube.com/watch?v=94n4sm7Ip_E")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func cardioVideo(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.youtube.com/watch?v=ji2v1a6VwL8")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
