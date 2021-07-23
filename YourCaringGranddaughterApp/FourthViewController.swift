67c907d8c1038b16a43a314b631fe120581ef149
Skylar A <skylar.arce@yahoo.com>
2021-07-23 10:24:31 -0700

    added connections to fourth view controller

--- YourCaringGranddaughterApp/FourthViewController.swift
+++ YourCaringGranddaughterApp/FourthViewController.swift
@@ -26,17 +26,23 @@ class FourthViewController: UIViewController {
     }
     */
     
-    @IBAction func buttonOne(_ sender: UIButton) {
-        UIApplication.shared.open(URL(string:"https://www.parkmanorapts.com/blog/1542057880/5-reasons-why-seniors-should-embrace-technology#:~:text=In%20short%2C%20technology%20isn't,higher%20quality%20of%20life%20overall.")! as URL, options: [:],completionHandler: nil)        
+    @IBAction func buttonOneTapped(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.parkmanorapts.com/blog/1542057880/5-reasons-why-seniors-should-embrace-technology#:~:text=In%20short%2C%20technology%20isn't,higher%20quality%20of%20life%20overall.")! as URL, options: [:],completionHandler: nil)
+
     }
     
-    @IBAction func buttonTwo(_ sender: UIButton) {
+    
+    
+    @IBAction func buttonTwoTapped(_ sender: UIButton) {
         UIApplication.shared.open(URL(string:"https://aginginplace.org/how-to-become-tech-savvy-for-seniors-in-10-days/")! as URL, options: [:],completionHandler: nil)
     }
-    @IBAction func buttonThree(_ sender: UIButton) {
+    
+    
+    @IBAction func buttonThreeTapped(_ sender: UIButton) {
         UIApplication.shared.open(URL(string:"https://www.youtube.com/watch?v=h_YG8B_Wh6o&ab_channel=AmericanEnglish")! as URL, options: [:],completionHandler: nil)
     }
-    @IBAction func buttonFour(_ sender: UIButton) {
+    
+    @IBAction func buttonFourTapped(_ sender: UIButton) {
         UIApplication.shared.open(URL(string:"https://freshysites.com/web-design-development/most-popular-websites/")! as URL, options: [:],completionHandler: nil)
     }
     
